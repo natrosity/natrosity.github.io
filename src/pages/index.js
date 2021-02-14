00c87f9193e84dfdbcae0c22fa17fa3d1d10ad7c
teabolt <tomasbaltrunas0@gmail.com>
2021-02-14 15:33:59 +0000

    Add two column layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { css } from "@emotion/react";
 import * as React from "react";
 
 import Button from "natrosity-components/Button";
@@ -9,17 +10,41 @@ const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
-      <div>
-        <h1>The only rapper without a face tatt</h1>
-        <h1>
-          <span>Out Now</span>
-          <br />
-          No More Love Songs
-        </h1>
-        <Button>LISTEN NOW</Button>
-        <SocialMediaList />
-        <MusicPlatformList />
-        <SpotifyPlayer />
+      <div
+        css={css`
+          // Column layout
+          // Adapted from https://dev.to/drews256/ridiculously-easy-row-and-column-layouts-with-flexbox-1k01
+          .row {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            width: 100%;
+          }
+
+          .column {
+            display: flex;
+            flex-direction: column;
+            flex-basis: 100%;
+            flex: 1;
+          }
+        `}
+      >
+        <div class="row">
+          <div class="column">
+            <h1>The only rapper without a face tatt</h1>
+            <h1>
+              <span>Out Now</span>
+              <br />
+              No More Love Songs
+            </h1>
+            <Button>LISTEN NOW</Button>
+            <SocialMediaList />
+            <MusicPlatformList />
+          </div>
+          <div class="column">
+            <SpotifyPlayer />
+          </div>
+        </div>
       </div>
     </main>
   );
